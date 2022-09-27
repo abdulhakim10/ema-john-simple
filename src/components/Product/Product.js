c0f190ab489673714c3c1e049452d2a20552f3fd
syed abdul hakim <s.abdulhakim10@gmail.com>
2022-09-27 21:09:08 +0600

    product info and cart button are added in product component

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -2,10 +2,20 @@ import React from 'react';
 import './Product.css'
 
 const Product = (props) => {
-   const {name, img, seller, price, rating} = props.product;
+  // console.log(props)
+   const {name, img, seller, price, ratings} = props.product;
     return (
         <div className='product'>
           <img src={img} alt="" />
+          <div className="product-info">
+            <p className='product-name'>{name}</p>
+            <p className='product-name'>Price: ${price}</p>
+            <p><small>Manufacturer: {seller}</small></p>
+            <p><small>Rating: {ratings} stars</small></p>
+          </div>
+          <button className='cart-btn'>
+            <p>Add to Cart</p>
+          </button>
         </div>
     );
 };
