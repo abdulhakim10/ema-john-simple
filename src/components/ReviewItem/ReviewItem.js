e614894d22e8683f8e3b81c4e743dd5a6068b203
syed abdul hakim <s.abdulhakim10@gmail.com>
2022-10-09 01:44:22 +0600

    review items style and delete icon added

--- src/components/ReviewItem/ReviewItem.js
+++ src/components/ReviewItem/ReviewItem.js
@@ -1,8 +1,10 @@
+import { faTrashAlt } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
 import './ReviewItem.css'
 
 const ReviewItem = ({product}) => {
-    const {name, price, img, quantity} = product;
+    const {name, price, img, quantity, shipping} = product;
     return (
         <div className='review-item'>
             <div>
@@ -12,10 +14,13 @@ const ReviewItem = ({product}) => {
                 <div className="review-detail">
                     <p>{name}</p>
                     <p><small>Price: ${price}</small></p>
+                    <p><small>Shipping: ${shipping}</small></p>
                     <p><small>Quantity: {quantity}</small></p>
                 </div>
                 <div className="delete-container">
-                    <button>delete</button>
+                    <button className='dlt-btn'>
+                        <FontAwesomeIcon className='dlt-icon' icon={faTrashAlt}></FontAwesomeIcon>
+                    </button>
                 </div>
             </div>
         </div>
