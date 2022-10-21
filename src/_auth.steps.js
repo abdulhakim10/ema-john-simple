7045baefa344783620f2ae0682519157f20f6845
syed abdul hakim <s.abdulhakim10@gmail.com>
2022-10-22 01:49:02 +0600

    Setup Auth Context under UserContext to share auth info

--- src/_auth.steps.js
+++ src/_auth.steps.js
@@ -3,6 +3,16 @@ AUTH SETUP
 1. Create firebase project
 2. Enable Web
 3. Enable sign in method
-4. Get firebase config in firebase.config.js file
-5. Export app from firebase.config.js file
+4. Install firebase
+5. Get firebase config in firebase.config.js file
+6. Export app from firebase.config.js file
+*/
+
+/*
+1. Create UserContext (Auth Context): UserContext --> component name
+UserContext provides AuthContext
+
+2. Create AuthContext 
+3. Set AuthContext.Provider
+4. Make sure to set the children
 */
